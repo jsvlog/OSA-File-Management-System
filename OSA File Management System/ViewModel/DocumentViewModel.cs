50b99e6c50a653b044c0bbe4ef8985ef14543345
JS <lizardojohnsonc@gmail.com>
2024-10-17 09:46:57 +0800

    working in ad form

--- OSA File Management System/ViewModel/DocumentViewModel.cs
+++ OSA File Management System/ViewModel/DocumentViewModel.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using OSA_File_Management_System.View;
 using OSA_File_Management_System.Model;
+using Org.BouncyCastle.Asn1.IsisMtt.X509;
+using OSA_File_Management_System.Commands;
+using Mysqlx.Crud;
 
 namespace OSA_File_Management_System.ViewModel
 {
@@ -20,7 +23,32 @@ namespace OSA_File_Management_System.ViewModel
         {
             documentServices = new DocumentServices();
             DocumentList = documentServices.GetAllDocuments();
+            showAddForm = new RelayCommand(OpenAddDocumentForm);
         }
+
+
+        private RelayCommand showAddForm;
+
+        public RelayCommand ShowAddForm
+        {
+            get { return showAddForm; }
+        }
+
+        private void OpenAddDocumentForm()
+        {
+            // Create the popup window
+            AddFormInventory popup = new AddFormInventory();
+
+            // Bind ViewModel to the popup window
+            popup.DataContext = new DocumentViewModel();
+
+            // Show the popup window
+            popup.ShowDialog();
+        }
+
+
+
+
 
 
     }
